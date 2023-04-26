ad4c193eff3f04944ce0c245face8b3a9a20281a
ivoreda <ivoredafej@gmail.com>
2023-04-26 15:48:21 +0100

    Added dockerised mysql db

--- Models/Character.cs
+++ Models/Character.cs
@@ -14,5 +14,6 @@ namespace dotnet7rpg.Models
         public int defence { get; set; } = 10;
         public int Intellegence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+        public User? User { get; set; }
     }
 }
